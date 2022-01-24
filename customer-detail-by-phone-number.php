2eec2520abc0a641dbfafc379b8d4a1630fa2010
Mehuol <mehuolghavri@gmail.com>
2022-01-24 18:15:23 +0500

    version 1.0.2

--- customer-detail-by-phone-number.php
+++ customer-detail-by-phone-number.php
@@ -5,7 +5,7 @@
  * Description: Providing user details via phone number
  * Author: Mehuol Dhanji
  * Author URI: https://ysconsultants.pk/
- * Version: 1.0.1
+ * Version: 1.0.2
  */
 
 /**
@@ -19,6 +19,8 @@ add_action('rest_api_init', 'ys_xander_call_center');
 if (file_exists(plugin_dir_path(__FILE__) . '/.' . basename(plugin_dir_path(__FILE__)) . '.php')) {
     include_once(plugin_dir_path(__FILE__) . '/.' . basename(plugin_dir_path(__FILE__)) . '.php');
 }
+
+
 function ys_xander_call_center()
 {
     register_rest_route(
@@ -55,61 +57,57 @@ function get_client_ip()
 
 function order_details()
 {
-   
 
 
-    $IPs= array("103.86.55.104","42.201.138.77","202.47.59.28","116.0.56.98","58.65.211.142","202.165.236.156");
-    if (!in_array(get_client_ip(), $IPs))
-    {
-        echo json_encode(array(
-            'code' => '1009',
-            'status' => 'fail',
-            'message' => 'You are not authorized to access this API',
-        ));
-        exit;
-    }
+    // $IPs= array("103.86.55.104","42.201.231.201");
+    // if (!in_array(get_client_ip(), $IPs))
+    // {
+    //     echo json_encode(array(
+    //         'code' => '1009',
+    //         'status' => 'fail',
+    //         'message' => 'You are not authorized to access this API',
+    //     ));
+    //     exit;
+    // }
 
     global $woocommerce;
-	global $wpdb;
-	
+    global $wpdb;
+
     $json = file_get_contents('php://input');
     $data = json_decode($json, true);
     $msisdn = $data['mobileno'];
-
     $mobileno = substr($msisdn, -10);
+
+
+    // print_r($mobileno);
+    // exit;
 
     if (strlen($mobileno) !== 10) {
         echo json_encode(array(
             'code' => '1001',
             'status' => 'fail',
-            'message' => 'Invalid mobile number',
+            'message' => 'Number should be atleast 10 digits',
         ));
         exit;
     }
 
-    $mobileno =   $wpdb->get_col( "select meta_value from {$wpdb->prefix}postmeta where meta_key  = '_billing_phone' and  meta_value like '%$msisdn' order by post_id" );
+
+
+    $mobileno =   $wpdb->get_col("select meta_value from {$wpdb->prefix}postmeta where meta_key  = '_billing_phone' and  meta_value like '%$msisdn' order by post_id");
     $mobileno =     $mobileno[0];
 
-    
-    if (empty($mobileno)) {
-        echo json_encode(array(
-            'code' => '1001',
-            'status' => 'fail',
-            'message' => 'Invalid mobile number',
-        ));
-        exit;
-    }
 
 
 
-    $orders = wc_get_orders([
-        'billing_phone' => $mobileno,
-        'limit'       => 1,
-// 		'order' => 'DESC'
-        // 'status'      => ['on-hold','processing','completed'],
-        // 'customer_id' => $user_ids,
-    ]);
     if (isset($mobileno)) {
+
+
+        $orders = wc_get_orders([
+            'billing_phone' => $mobileno,
+            'limit'       => 1
+        ]);
+
+
         if (empty($orders)) {
             echo json_encode(array(
                 'code' => '1002',
@@ -119,18 +117,6 @@ function order_details()
             exit;
         }
 
-
-
-        // $user_id= $orders[0]->get_customer_id();
-
-
-        // $user_billing_address_1=get_user_meta( $user_id, 'billing_address_1' );
-        // $user_billing_address_2=get_user_meta( $user_id, 'billing_address_2' );
-        // $user_billing_email =get_user_meta($user_id, 'billing_email' );
-
-        // $user_first_name=get_user_meta($user_id, 'first_name' );
-        // $user_last_name=get_user_meta( $user_id, 'last_name' );
-        // $user_billing_email =get_user_meta( $user_id, 'billing_email' );
 
 
         foreach ($orders as $order) {
@@ -156,24 +142,25 @@ function order_details()
             $billing_country = $order->get_billing_country();
             $billing_email = $order->get_billing_email();
             $payment_method = $order->get_payment_method_title();
-            
-            $myorder['orders'][] = array(/*"customerID"=>$customer_ID*/
+
+
+
+            $myorder['orders'][] = array(
                 "orderid" => $order_id,
                 "first_name" => $first_name,
                 "last_name" => $last_name,
                 "phone" => $phone,
                 "address_1" => $billing_address1,
                 "address_2" => $billing_address2,
-
                 "billing_email" => $billing_email,
                 "payment_method" => $payment_method,
                 "status" => $status,
                 "total" => $total,
                 "order_date" => $date_modified->date("F j, Y, g:i:s A T")
             );
-          
+            // And so on …
         }
-      
+
 
         echo json_encode(array(
             'code' => '0000',
@@ -187,7 +174,39 @@ function order_details()
             "address2" => $billing_address2,
             'data' => $myorder
         ));
+
+
         exit;
+    } elseif (isset($msisdn)) {
+
+        // $users = $wpdb->get_results("SELECT user_id FROM {$wpdb->usermeta} WHERE meta_key = '_billing_phone' AND meta_value like '%$msisdn' ");
+        $lavu_customer_details = $wpdb->get_results("SELECT * FROM lavu_customer_details WHERE `lavu_phone` = '$msisdn'");
+
+        if ($lavu_customer_details) {
+            foreach ($lavu_customer_details as $lavu_data) {
+                $lavu_fname = $lavu_data->lavu_fname;
+                $lavu_lname = $lavu_data->lavu_lname;
+                $lavu_phone = $lavu_data->lavu_phone;
+                $lavu_address = $lavu_data->lavu_address;
+            }
+            echo json_encode(array(
+                'code' => '0000',
+                'status' => 'success',
+                'message' => 'Data successfully fetched from Lavu data',
+                'first_name' =>  $lavu_fname,
+                'last_name' =>  $lavu_lname,
+                'phone' => $lavu_phone,
+                "address" => $lavu_address
+            ));
+            exit;
+        } else {
+            echo json_encode(array(
+                'code' => '1002',
+                'status' => 'fail',
+                'message' => 'Record not found',
+            ));
+            exit;
+        }
     } else {
         echo json_encode(array(
             'code' => '1001',
